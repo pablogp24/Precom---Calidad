c877e70ab99e62bfd70595def0a9d6ce8486abd6
pablogp24 <pablo.razrhd@gmail.com>
2026-08-28 21:44:52 -0300

    Configurar Supabase V29

---
+++ v29-central/online-config.js
@@ -0,0 +1,5 @@
+window.PECOM_ONLINE = {
+  supabaseUrl: "https://vbdfspjibkyjyailhraa.supabase.co",
+  supabaseAnonKey: "sb_publishable_6uBEk-7IhpJ1g2Vt_GKSeg_HmawNkP3",
+  adminEmail: "pablo.perez@pecomenergia.com.ar"
+};
