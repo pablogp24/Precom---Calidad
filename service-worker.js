0a2d14e7374b3dc4856b9a1f0f4bd981b94a8d89
pablogp24 <pablo.razrhd@gmail.com>
2026-08-29 16:57:25 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,74 @@
+const CACHE = 'flecap-v29-offline-multiuser-v1';
+
+const CORE = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './online-config.js'
+];
+
+self.addEventListener('install', event =>
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(async cache => {
+        for (const url of CORE) {
+          try {
+            await cache.add(url);
+          } catch (e) {}
+        }
+      })
+      .then(() => self.skipWaiting())
+  )
+);
+
+self.addEventListener('activate', event =>
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  )
+);
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE)
+            .then(cache => cache.put('./index.html', copy));
+          return response;
+        })
+        .catch(() =>
+          caches.match('./index.html')
+            .then(response => response || caches.match('./'))
+        )
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      const network = fetch(event.request)
+        .then(response => {
+          if (response && response.status < 400) {
+            const copy = response.clone();
+            caches.open(CACHE)
+              .then(cache => cache.put(event.request, copy));
+          }
+          return response;
+        })
+        .catch(() => cached);
+
+      return cached || network;
+    })
+  );
+});
