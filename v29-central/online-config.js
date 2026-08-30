d9f5a6c6181cbe5813408d10c627ed00b938c6ab
pablogp24 <pablo.razrhd@gmail.com>
2026-08-30 19:26:04 -0300

    Delete v29-central directory

--- v29-central/online-config.js
+++
@@ -1,5 +0,0 @@
-window.PECOM_ONLINE = {
-  supabaseUrl: "https://fujtkmsmoeewfelhpckh.supabase.co",
-  supabaseAnonKey: "sb_publishable_okl7QJvuSMDWa_aw2yUABw_XFjc39IT",
-  adminEmail: "pablo.perez@pecomenergia.com.ar"
-};
