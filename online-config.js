eb8d54f287ec61b23a877b2c78ff569524bff37e
pablogp24 <pablo.razrhd@gmail.com>
2026-09-19 10:34:03 -0300

    Add files via upload

--- online-config.js
+++ online-config.js
@@ -2,7 +2,6 @@
 window.PECOM_ONLINE = {
   supabaseUrl: "https://vbdfspjibkyjyailhraa.supabase.co",
   supabaseAnonKey: "sb_publishable_6uBEk-7IhpJ1g2Vt_GKSeg_HmawNkP3",
-  adminEmail: "pablo.perez@pecomenergia.com.ar"
 };
 
 // Alias de compatibilidad. El core actual usa PECOM_ONLINE.
